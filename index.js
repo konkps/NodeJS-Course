1ae7bdd6c3949c46f911fbf97bee7eb7f03d2ee3
kostas kappas <kostas@pikei.io>
2021-11-27 23:45:46 +0200

    building html templates and api routing

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-const fs = require('fs');
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
 
-// const hello = 'hello world';
-// console.log(hello);
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
 
-const textIn =fs.readFileSync('./txt/input.txt','utf-8')
-console.log(textIn);
+  switch (pathName) {
+    case "/":
+      res.end("Hello from the server!\nThis is Home page");
+      break;
+    case "/overview":
+      res.end("This is Overview Page!");
+      break;
+    case "/product":
+      res.end("This is the Product Page!");
+      break;
+    case "/api":
+      fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+        const productData = JSON.parse(data);
+        res.writeHead(200, {
+          "Content-type": "application/json",
+        });
+        res.end(data);
+      });
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
-fs.writeFileSync('./txt/output.txt',textOut)
-console.log('File written!');
+      break;
+    default:
+      res.writeHead(404, {
+        "Content-type": "text/html",
+        "my-own-header": "hello-world",
+      });
+      res.end("<h1>Page could not be found!</h1>");
+    //   res.writeHead(404);
+    //   res.end("Page could not be found!");
+  }
+});
+
+const port = 8000;
+server.listen(port, "127.0.0.1", () => {
+  console.log(`Server listening on ${port}`);
+});
