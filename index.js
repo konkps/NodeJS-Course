8387806db1602c1cbd2f709ff5316b9b07991f26
kostas kappas <kostas@pikei.io>
2021-11-28 20:49:01 +0200

    dynamic templates & routing

--- index.js
+++ index.js
@@ -2,29 +2,55 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const pathName = req.url;
+const replaceTemplate = require("./modules/replaceTemplate");
 
-  switch (pathName) {
+const templOverview = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  "utf-8"
+);
+const templCard = fs.readFileSync(`${__dirname}/templates/card.html`, "utf-8");
+const templProduct = fs.readFileSync(
+  `${__dirname}/templates/product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+// Create SERVER
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+  // const pathname = req.url;
+  let output;
+  switch (pathname) {
     case "/":
       res.end("Hello from the server!\nThis is Home page");
       break;
+    // Overview Page
     case "/overview":
-      res.end("This is Overview Page!");
-      break;
-    case "/product":
-      res.end("This is the Product Page!");
-      break;
-    case "/api":
-      fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
-        const productData = JSON.parse(data);
-        res.writeHead(200, {
-          "Content-type": "application/json",
-        });
-        res.end(data);
-      });
+      res.writeHead(200, { "Content-type": "text/html" });
 
+      const cardsHtml = dataObj
+        .map((item) => replaceTemplate(templCard, item))
+        .join("");
+      output = templOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+      res.end(output);
       break;
+    // Product Page
+    case "/product":
+      const product = dataObj[query.id];
+      output = replaceTemplate(templProduct, product);
+
+      res.writeHead(200, { "Content-type": "text/html" });
+      res.end(output);
+      break;
+    // API
+    case "/api":
+      res.writeHead(200, { "Content-type": "application/json" });
+      res.end(data);
+      break;
+    // Not Found
     default:
       res.writeHead(404, {
         "Content-type": "text/html",
